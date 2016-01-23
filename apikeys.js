f5b73935c57e42fc6bef5886d0e14fe24248192f
Liu Yuyang <lhtlyy@gmail.com>
2016-01-23 22:16:50 +0800

    new file:   apikeys.js

---
+++ apikeys.js
@@ -0,0 +1,4 @@
+module.exports.turingRobotApiKey = '5adc1e506759dab65ace7f2774043599';
+module.exports.turingBaiduRobotApiKey = '6053e172b7994b684aadfd4ae0841510';
+module.exports.turingBaiduRobotKey = '879a6cb3afb84dbf4fc84a1df2ab7319';
+module.exports.baiduDirectApiKey = '57E372d5405486b9479793899d231bb2';
